ca90c511111deea66544af1457b9236c08078ef8
kazenetu <rakudafactory@gmail.com>
2018-04-22 19:43:25 +0900

    ログインのテストボタンを実装

--- WinHttpConnectJsonLib/Form1.Designer.cs
+++ WinHttpConnectJsonLib/Form1.Designer.cs
@@ -28,34 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(48, 43);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(194, 44);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "認証なしWebAPI呼び出し";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(509, 393);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(48, 43);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(194, 44);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "認証なしWebAPI呼び出し";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(48, 116);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(194, 44);
+      this.button2.TabIndex = 1;
+      this.button2.Text = "ログイン成功";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(509, 393);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-    }
+    private System.Windows.Forms.Button button2;
+  }
 }
 
--- WinHttpConnectJsonLib/Form1.cs
+++ WinHttpConnectJsonLib/Form1.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using DataTransferObjects.Request;
+using DataTransferObjects.Response;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Deployment.Application;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,6 +22,48 @@ namespace WinHttpConnectJsonLib
     }
 
     /// <summary>
+    /// WebAPIのルートURLを取得
+    /// </summary>
+    /// <param name="setApiPath">APIパスの追加</param>
+    /// <returns>APIのルートURL</returns>
+    private string getWebApiRootAddress(bool setApiPath)
+    {
+      var url = string.Empty;
+
+      if (ApplicationDeployment.IsNetworkDeployed)
+      {
+        // ClickOnce実行時のURL
+        var updateLocation = ApplicationDeployment.CurrentDeployment.UpdateLocation;
+        url = string.Format("{0}://{1}:{2}/", updateLocation.Scheme, updateLocation.Host, updateLocation.Port);
+      }
+      else
+      {
+        // デバッグ実行時のURL
+        var args = Environment.GetCommandLineArgs();
+        if (args.Length >= 2)
+        {
+          url = args[1];
+          if (url[url.Length - 1] != '/')
+          {
+            url += "/";
+          }
+        }
+        else
+        {
+          url = "http://localhost:8080/";
+        }
+      }
+
+      // APIパス追加
+      if (setApiPath)
+      {
+        url += "api/";
+      }
+
+      return url;
+    }
+
+    /// <summary>
     /// 認証なしのWebAPIの呼び出し
     /// </summary>
     /// <param name="sender"></param>
@@ -30,6 +75,18 @@ namespace WinHttpConnectJsonLib
       var url = "http://localhost:5000/api/account/testPost";
       var result = HttpConnectLib.Post<object>(url, param);
       MessageBox.Show(result.ToString());
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      // WebAPIのルートURLを取得
+      var rootURL = getWebApiRootAddress(false);
+
+      // GetTokenメソッド発行
+      HttpConnectLib.GetToken(string.Format("{0}", rootURL));
+
+      var param = new LoginRequest() {ID="test",Password="test" };
+      var results = HttpConnectLib.Post<LoginResponse>(string.Format("{0}account/login", getWebApiRootAddress(true)), param);
     }
   }
 }
