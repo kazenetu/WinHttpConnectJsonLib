3cdcf3735bf694346435ec752641dcf6b8087572
kazenetu <rakudafactory@gmail.com>
2018-04-24 20:58:41 +0900

    例外エラーをキャッチできるように修正

--- WinHttpConnectJsonLib/Form1.cs
+++ WinHttpConnectJsonLib/Form1.cs
@@ -70,11 +70,18 @@ namespace WinHttpConnectJsonLib
     /// <param name="e"></param>
     private void button1_Click(object sender, EventArgs e)
     {
-      var param = new Dictionary<string, object>() { { "id", "test" }, { "test", "test" } };
+      try
+      {
+        var param = new Dictionary<string, object>() { { "id", "test" }, { "test", "test" } };
 
-      var url = "http://localhost:5000/api/account/testPost";
-      var result = HttpConnectLib.Post<object>(url, param);
-      MessageBox.Show(result.ToString());
+        var url = "http://localhost:5000/api/account/testPost";
+        var result = HttpConnectLib.Post<object>(url, param);
+        MessageBox.Show(result.ToString());
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
     }
 
     /// <summary>
@@ -84,22 +91,29 @@ namespace WinHttpConnectJsonLib
     /// <param name="e"></param>
     private void button2_Click(object sender, EventArgs e)
     {
-      // WebAPIのルートURLを取得
-      var rootURL = getWebApiRootAddress(false);
+      try
+      {
+        // WebAPIのルートURLを取得
+        var rootURL = getWebApiRootAddress(false);
 
-      // GetTokenメソッド発行
-      HttpConnectLib.GetToken(string.Format("{0}", rootURL));
+        // GetTokenメソッド発行
+        HttpConnectLib.GetToken(string.Format("{0}", rootURL));
 
-      // ログインWebAPI発行
-      var param = new LoginRequest() {ID="test",Password="test" };
-      var results = HttpConnectLib.Post<LoginResponse>(string.Format("{0}account/login", getWebApiRootAddress(true)), param);
+        // ログインWebAPI発行
+        var param = new LoginRequest() { ID = "test", Password = "test" };
+        var results = HttpConnectLib.Post<LoginResponse>(string.Format("{0}account/login", getWebApiRootAddress(true)), param);
 
-      // 結果を表示
-      var sb = new StringBuilder();
-      sb.AppendLine($"result = {results.Result}");
-      sb.AppendLine($"message = {results.ErrorMessage}");
-      sb.AppendLine($"data = {results.ResponseData?.Name}");
-      MessageBox.Show(sb.ToString());
+        // 結果を表示
+        var sb = new StringBuilder();
+        sb.AppendLine($"result = {results.Result}");
+        sb.AppendLine($"message = {results.ErrorMessage}");
+        sb.AppendLine($"data = {results.ResponseData?.Name}");
+        MessageBox.Show(sb.ToString());
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
     }
 
     /// <summary>
@@ -109,22 +123,29 @@ namespace WinHttpConnectJsonLib
     /// <param name="e"></param>
     private void button3_Click(object sender, EventArgs e)
     {
-      // WebAPIのルートURLを取得
-      var rootURL = getWebApiRootAddress(false);
+      try
+      {
+        // WebAPIのルートURLを取得
+        var rootURL = getWebApiRootAddress(false);
 
-      // GetTokenメソッド発行
-      HttpConnectLib.GetToken(string.Format("{0}", rootURL));
+        // GetTokenメソッド発行
+        HttpConnectLib.GetToken(string.Format("{0}", rootURL));
 
-      // ログインWebAPI発行
-      var param = new LoginRequest() { ID = "test", Password = "test1" };
-      var results = HttpConnectLib.Post<LoginResponse>(string.Format("{0}account/login", getWebApiRootAddress(true)), param);
+        // ログインWebAPI発行
+        var param = new LoginRequest() { ID = "test", Password = "test1" };
+        var results = HttpConnectLib.Post<LoginResponse>(string.Format("{0}account/login", getWebApiRootAddress(true)), param);
 
-      // 結果を表示
-      var sb = new StringBuilder();
-      sb.AppendLine($"result = {results.Result}");
-      sb.AppendLine($"message = {results.ErrorMessage}");
-      sb.AppendLine($"data = {results.ResponseData?.Name}");
-      MessageBox.Show(sb.ToString());
+        // 結果を表示
+        var sb = new StringBuilder();
+        sb.AppendLine($"result = {results.Result}");
+        sb.AppendLine($"message = {results.ErrorMessage}");
+        sb.AppendLine($"data = {results.ResponseData?.Name}");
+        MessageBox.Show(sb.ToString());
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
     }
   }
 }
